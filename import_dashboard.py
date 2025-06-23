138483698e49987729df6cd42dbd62f066bab202
na-tarasova <nataly.verba90@gmail.com>
2025-06-23 22:53:40 +0400

    added files for local version

---
+++ import_dashboard.py
@@ -0,0 +1,3 @@
+curl \                                                       
+-H 'x-api-key: mb_pVJ4TI96TFhoypdQ6/D4mKfGkjJKWDgWByug0TD8PPQ=' \
+-X GET 'http://localhost:3000/api/dashboard/3' || jq
